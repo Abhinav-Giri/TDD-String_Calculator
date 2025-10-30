e8f5dbeb0ad51e6b84abd7704e69792008208aa8
Abhinav-Giri <iamabhinavgiri@gmail.com>
2025-10-30 21:11:56 +0530

    test: Add support for newline delimiters

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,7 +1,7 @@
 export class StringCalculator {
   add(numbers: string): number {
     if (numbers === '') return 0;
-    return numbers.split(',')
+    return numbers.split(/[,\n]/)
       .map(num => parseInt(num))
       .reduce((sum, num) => sum + num, 0);
   }
